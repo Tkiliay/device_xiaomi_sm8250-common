39f59ceb31685872acda4212cdd2c8a813a4e965
Mathieu Chartier <mathieuc@google.com>
2023-02-11 10:17:52 +0000

    sm8250-common: Speed profile services and wifi-service to reduce RAM and storage

--- kona.mk
+++ kona.mk
@@ -95,6 +95,9 @@ PRODUCT_PACKAGES += \
     otapreopt_script
 endif
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # ANT+
 PRODUCT_PACKAGES += \
     com.dsi.ant@1.0.vendor
