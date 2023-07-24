498cc4f2d872a3fe97643505c41ca797f0067653
Gustavo Mendes <gusttavo.me@outlook.com>
2023-07-24 18:33:31 -0300

    sm8250-common: parts: restore doze - Left behind in decommunization. - From https://github.com/Alioth-Lab/device_xiaomi_sm8250-common/tree/thirteen/parts
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -28,6 +28,7 @@ import android.util.Log;
 import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.dirac.DiracUtils;
+import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.utils.FileUtils;
@@ -50,6 +51,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         } catch (Exception e) {
             Log.d(TAG, "Dirac is not present in system");
         }
+        DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
         RefreshUtils.startService(context);
 
